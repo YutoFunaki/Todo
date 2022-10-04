c86d39f8fb09b7b61fed6556933dfd24ec1adcad
YutoFunaki <104195830+YutoFunaki@users.noreply.github.com>
2022-10-05 06:07:20 +0900

    add cell, remove cell. but no signal cell trush.

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -12,11 +12,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
 
     var to = [String]()
-    //表示するリスト
+    var todoList = [String]()
+    //インスタンス生成
+    let userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        func viewDidLoad() {
+            super.viewDidLoad()
+            //データ読み込み
+            if let storedTodoList = userDefaults.array(forKey: "todoList") as? [String] {
+                todoList.append(contentsOf: storedTodoList)
+            }
+        }
     }
     
     
@@ -35,17 +43,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBAction func addTodo(_ sender: Any) {
         let alertController = UIAlertController(title: "追加", message: "やることをを記入してください", preferredStyle: UIAlertController.Style.alert)
         alertController.addTextField(configurationHandler: nil)
+        
+        //okAction
         let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default){
             (acrion: UIAlertAction) in
             if let textField = alertController.textFields?.first {
                 self.to.insert(textField.text!, at: 0)
                 self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableView.RowAnimation.right)
+                
+                    self.userDefaults.set(self.to, forKey: "to")
             }
+            
         }
         alertController.addAction(okAction)
+        
+        //cancelAction
         let cancelButton = UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.cancel, handler: nil)
         alertController.addAction(cancelButton)
         present(alertController, animated: true, completion: nil)
+        
+        //settingCell
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+                let todoTitle = to[indexPath.row]
+                cell.textLabel?.text = todoTitle
+                return cell
+            }
+        
+        //trushCell
+        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == UITableViewCell.EditingStyle.delete {
+                to.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+                
+                userDefaults.set(to, forKey: "to")
+            }
+        }
         
     }
     

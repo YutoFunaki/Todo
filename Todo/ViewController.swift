4bba16ae06968b30b971d870058617ab4ac81eb8
YutoFunaki <104195830+YutoFunaki@users.noreply.github.com>
2022-10-10 03:33:30 +0900

    Finish! Error is tableview in func addTodo.

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -8,37 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet weak var tableView: UITableView!
     
+    
+    @IBOutlet weak var tableView: UITableView!
 
     var to = [String]()
-    var todoList = [String]()
     //インスタンス生成
     let userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        func viewDidLoad() {
             super.viewDidLoad()
             //データ読み込み
-            if let storedTodoList = userDefaults.array(forKey: "todoList") as? [String] {
-                todoList.append(contentsOf: storedTodoList)
+            if let storedTodoList = userDefaults.array(forKey: "to") as? [String] {
+                to.append(contentsOf: storedTodoList)
             }
-        }
     }
     
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return to.count
-    }
-    //テーブルに表示するセルの数を指定
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel!.text = to[indexPath.row]
-        return cell
-        //テーブルのセルの内容を決める
-    }
     
     @IBAction func addTodo(_ sender: Any) {
         let alertController = UIAlertController(title: "追加", message: "やることをを記入してください", preferredStyle: UIAlertController.Style.alert)
@@ -51,7 +36,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 self.to.insert(textField.text!, at: 0)
                 self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableView.RowAnimation.right)
                 
-                    self.userDefaults.set(self.to, forKey: "to")
+                self.userDefaults.set(self.to, forKey: "to")
             }
             
         }
@@ -62,26 +47,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         alertController.addAction(cancelButton)
         present(alertController, animated: true, completion: nil)
         
-        //settingCell
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
-                let todoTitle = to[indexPath.row]
-                cell.textLabel?.text = todoTitle
-                return cell
-            }
+    }
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+            return to.count
+        }
         
-        //trushCell
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel!.text = to[indexPath.row]
+            return cell
+            //テーブルのセルの内容を決める
+        }
+       
+
+        //スワイプしたセルを削除
         func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
             if editingStyle == UITableViewCell.EditingStyle.delete {
-                to.remove(at: indexPath.row)
+            to.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
                 
                 userDefaults.set(to, forKey: "to")
             }
         }
         
-    }
-    
-    
 }
 
